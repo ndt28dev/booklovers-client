d69da946d0f379c7da3548b2bdca4893d415a3ea
Nguyễn Duy Thuần <ndt28dev@gmail.com>
2026-03-12 17:21:33 +0700

    done listContact

--- src/containers/admin/main/content/contacts/Contacts.jsx
+++ src/containers/admin/main/content/contacts/Contacts.jsx
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "../../../../../redux/slices/contactSlice";
+import MyLayoutAdmin from "../../../../../components/mylayout/MyLayoutAdmin";
+import { Col } from "react-bootstrap";
+import MyDataTable from "../../../../../components/mytable/MyDataTable";
+import { formatDate } from "../../../../../utils/format";
 
 const Contacts = () => {
-  return <div>Contact</div>;
+  const dispatch = useDispatch();
+
+  const { data, pagination } = useSelector(
+    (state) => state.contact.listContact
+  );
+  const page = pagination?.page || 1;
+  const limit = pagination?.limit || 10;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = pagination?.totalPages || 0;
+
+  useEffect(() => {
+    dispatch(
+      fetchContacts({
+        page: currentPage,
+        limit: 10,
+      })
+    );
+  }, [currentPage]);
+
+  const handlePageChange = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  const columns = [
+    { title: "STT", style: { width: "3%", textAlign: "center" } },
+    { title: "Họ và tên", style: { width: "16%" } },
+    { title: "Email", style: { width: "10%" } },
+    { title: "Số điện thoại", style: { width: "12%", textAlign: "center" } },
+    { title: "Tin nhắn" },
+    { title: "Ngày tạo", style: { width: "6%", textAlign: "center" } },
+    // { title: "Thao tác", style: { width: "10%", textAlign: "center" } },
+  ];
+
+  const renderRow = (contact, index) => (
+    <tr key={index}>
+      <td className="text-center align-middle d-none">{contact.id}</td>
+      <td className="text-center align-middle">
+        {(page - 1) * limit + index + 1}
+      </td>
+      <td className="align-middle">{contact.name}</td>
+      <td className="align-middle">{contact.email}</td>
+      <td className="align-middle text-center">{contact.phone}</td>
+      <td className="align-middle text-center">{contact.message}</td>
+      <td className="align-middle text-center">
+        {formatDate(contact.created_at)}
+      </td>
+    </tr>
+  );
+
+  return (
+    <MyLayoutAdmin title="Danh sách phản hồi">
+      <div style={{ minHeight: "722px" }}>
+        <MyDataTable
+          columns={columns}
+          data={data}
+          renderRow={renderRow}
+          pagination={pagination}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </MyLayoutAdmin>
+  );
 };
 
 export default Contacts;
--- src/redux/slices/contactSlice.js
+++ src/redux/slices/contactSlice.js
@@ -1,6 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import API_URL from "../../config/api";
 
+// CREATE CONTACT
 export const createContact = createAsyncThunk(
   "contact/createContact",
   async (contactData, thunkAPI) => {
@@ -15,31 +17,81 @@ export const createContact = createAsyncThunk(
   }
 );
 
+// FETCH CONTACTS
+export const fetchContacts = createAsyncThunk(
+  "contact/fetchContacts",
+  async ({ page = 1, limit = 10 }, thunkAPI) => {
+    try {
+      const res = await axios.get(
+        `${API_URL}/api/contacts?page=${page}&limit=${limit}`
+      );
+
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response?.data?.message || "Lỗi khi lấy danh sách liên hệ"
+      );
+    }
+  }
+);
+
 const initialState = {
-  submitSuccess: false,
-  submitError: null,
+  listContact: {
+    data: [],
+    pagination: null,
+    error: null,
+  },
+
+  createContact: {
+    isSubmitting: false,
+    success: false,
+    error: null,
+  },
 };
 
 const contactSlice = createSlice({
   name: "contact",
   initialState,
+
   reducers: {
     resetContactStatus: (state) => {
-      state.submitSuccess = false;
-      state.submitError = null;
+      state.createContact.success = false;
+      state.createContact.error = null;
+      state.createContact.isSubmitting = false;
     },
   },
+
   extraReducers: (builder) => {
     builder
-      .addCase(createContact.fulfilled, (state, action) => {
-        state.submitSuccess = true;
+
+      // CREATE CONTACT
+      .addCase(createContact.pending, (state) => {
+        state.createContact.isSubmitting = true;
+        state.createContact.error = null;
       })
+
+      .addCase(createContact.fulfilled, (state) => {
+        state.createContact.isSubmitting = false;
+        state.createContact.success = true;
+      })
+
       .addCase(createContact.rejected, (state, action) => {
-        state.isSubmitting = false;
-        state.submitError = action.payload;
+        state.createContact.isSubmitting = false;
+        state.createContact.error = action.payload;
+      })
+
+      // FETCH CONTACTS
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.listContact.pagination = action.payload.pagination;
+        state.listContact.data = action.payload.data;
+      })
+
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.listContact.error = action.payload;
       });
   },
 });
 
 export const { resetContactStatus } = contactSlice.actions;
+
 export default contactSlice.reducer;
