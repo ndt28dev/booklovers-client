2564ebeadee3933e7b7ad3a374e68e4e8986a9fe
Nguyễn Duy Thuần <ndt28dev@gmail.com>
2026-03-22 02:15:27 +0700

    load all category in settingCategory

--- src/containers/admin/main/content/settings/content/category/SettingCategory.jsx
+++ src/containers/admin/main/content/settings/content/category/SettingCategory.jsx
@@ -1,20 +1,66 @@
-import MyDataTable from "../../../../../../../components/mytable/MyDataTable";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 const SettingCategory = () => {
+  const { categories: data } = useSelector((state) => state.category);
+  const [openIds, setOpenIds] = useState([]);
+
+  const toggle = (id) => {
+    setOpenIds((prev) =>
+      prev.includes(id) ? prev.filter((i) => i !== id) : [...prev, id]
+    );
+  };
+
   return (
     <div>
       <h5 style={{ color: "#E35765" }}>Danh sách danh mục</h5>
-      <div style={{ minHeight: "722px", marginTop: "10px" }}>
-        <MyDataTable
-        //   columns={columns}
-        //   data={listPromotion}
-        //   renderRow={renderRow}
-        //   pagination={pagination}
-        //   currentPage={currentPage}
-        //   onPageChange={handlePageChange}
-        />
-      </div>
+
+      {data.map((cat) => (
+        <div key={cat.id} style={{ marginBottom: "10px", userSelect: "none" }}>
+          <div
+            onClick={() => toggle(cat.id)}
+            style={{
+              padding: "10px",
+              background: "#f5f5f5",
+              cursor: "pointer",
+              borderRadius: "6px",
+              fontWeight: "bold",
+            }}
+          >
+            <div className="d-flex align-items-center justify-content-between">
+              <div>
+                <span>
+                  {openIds.includes(cat.id) ? (
+                    <i class="bi bi-caret-down"></i>
+                  ) : (
+                    <i class="bi bi-caret-right"></i>
+                  )}{" "}
+                  {cat.name}{" "}
+                </span>
+                <span>({cat.subcategories.length})</span>
+              </div>
+            </div>
+          </div>
+
+          {openIds.includes(cat.id) && (
+            <div style={{ paddingLeft: "20px", marginTop: "5px" }}>
+              {cat.subcategories.map((sub) => (
+                <div
+                  key={sub.id}
+                  style={{
+                    padding: "6px 0",
+                    borderBottom: "1px solid #eee",
+                  }}
+                >
+                  - {sub.name}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
+
 export default SettingCategory;
